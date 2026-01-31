07ad9bc3855b668df2c55372261fb881758a61e9
Isaac Young <isaac3.young6@gmail.com>
2026-01-31 09:24:02 -0800

    mostly done with all behavior. add cart works, cart display is set, just need to do delete button and then adjust for desktop

--- script.js
+++ script.js
@@ -2,7 +2,29 @@ const navMenu = document.querySelector('#mobile-nav');
 const menuBtn = document.querySelector('#menu-btn')
 const overlay = document.querySelector('.dim-bg');
 
-menuBtn.addEventListener('click', () => {
+const dialog = document.querySelector('#shopping-cart')
+const cartBtn = document.querySelector('#cart-button');
+const cartProducts = document.querySelector('#cart-product-container');
+const checkoutBtn = document.querySelector('#checkout-button');
+
+const slides = document.querySelectorAll('.main__gallery-img');
+
+const quantity = document.querySelector('.main__quantity');
+const minusQuantityBtn = document.querySelector('.main__quantity-minus');
+const plusQuantityBtn = document.querySelector('.main__quantity-plus');
+const addToCartBtn = document.querySelector('#add-to-cart');
+
+
+
+
+
+
+let deleteBtns;
+let index = 0;
+let quant = +quantity.innerText;
+let prodCost = document.querySelector('#current-price')
+
+const openMenu = () => {
     let open = navMenu.classList.contains('open');
     navMenu.classList.toggle('open');
     if (!open) {
@@ -16,6 +38,81 @@ menuBtn.addEventListener('click', () => {
         menuBtn.setAttribute('aria-expanded', 'false');
         overlay.setAttribute('hidden', '')
     }
-    
-    
+}
+
+const showSlide = (n) => {
+    slides.forEach((slide, i) => {
+        slide.style.display = i === n ? 'block' : 'none'
+    })
+}
+showSlide(index)
+
+const changeSlides = (n) => {
+    index += n;
+    if (index >= slides.length) index = 0;
+    if (index < 0) index = slides.length - 1;
+    showSlide(index);
+}
+
+const disableMinusBtn = (n) => {
+    if (n === 0) {
+        minusQuantityBtn.setAttribute('disabled', '');
+        minusQuantityBtn.setAttribute('aria-disabled', 'true');
+        minusQuantityBtn.classList.add('disabled');
+    }else {
+        minusQuantityBtn.removeAttribute('disabled');
+        minusQuantityBtn.setAttribute('aria-disabled', 'false');
+        minusQuantityBtn.classList.remove('disabled');
+    }
+}
+
+const changeQuantity = (n) => {
+    quant += n;
+    quantity.innerText = quant;
+    disableMinusBtn(quant);
+}
+
+const addsProductToCart = () => {
+    let cost = +prodCost.innerText
+    const emptyMsg = document.querySelector('.header__cart-empty');
+    let empty = !emptyMsg.hasAttribute('hidden');
+    if (empty) {
+        emptyMsg.setAttribute('hidden', '');
+        checkoutBtn.removeAttribute('hidden')
+        cartProducts.style.display = 'flex'
+    };
+
+    cartProducts.innerHTML += 
+    `<div class="entry">
+        <img src="./images/image-product-1-thumbnail.jpg" alt="" class="header__cart-img">
+        <div class="header__cart-labels">
+        <p>Fall Limited Edition Sneakers</p>
+        <p><span>${cost}</span> x <span class="header__cart-quantity">${quant}</span><span class="header__cart-total tp3">$${(cost * quant)}</span></p>
+        </div>
+        <button class="header__cart-delete" id="delete-button"><img src="./images/icon-delete.svg" alt=""></button>
+    </div>`
+}
+
+
+
+
+/* const deletesProductFromCart = (btn) => {
+    btn.closest(document.querySelector('.entry')).innerHTML = ``;
+} */
+
+
+
+plusQuantityBtn.addEventListener('click', () => changeQuantity(1))
+minusQuantityBtn.addEventListener('click', () => changeQuantity(-1))
+
+menuBtn.addEventListener('click', () => {
+    openMenu()
+})
+
+addToCartBtn.addEventListener('click', () => {
+    addsProductToCart()
+})
+
+cartBtn.addEventListener('click', () => {
+    dialog.style.display = dialog.style.display === 'none' ? 'flex' : 'none'
 })
