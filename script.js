3b7e8417b62f5450e9729041b170b6968730c3fb
Isaac Young <isaac3.young6@gmail.com>
2026-02-02 17:11:57 -0800

    most functionality is done. Add/delete to cart

--- script.js
+++ script.js
@@ -2,7 +2,28 @@ const navMenu = document.querySelector('#mobile-nav');
 const menuBtn = document.querySelector('#menu-btn')
 const overlay = document.querySelector('.dim-bg');
 
-menuBtn.addEventListener('click', () => {
+const dialog = document.querySelector('#shopping-cart')
+const cartBtn = document.querySelector('#cart-button');
+const cartProducts = document.querySelector('#cart-product-container');
+const checkoutBtn = document.querySelector('#checkout-button');
+const prodCost = document.querySelector('#current-price');
+const emptyMsg = document.querySelector('.header__cart-empty');
+
+const slides = document.querySelectorAll('.main__gallery-img');
+
+const quantity = document.querySelector('.main__quantity');
+const minusQuantityBtn = document.querySelector('.main__quantity-minus');
+const plusQuantityBtn = document.querySelector('.main__quantity-plus');
+const addToCartBtn = document.querySelector('#add-to-cart');
+
+
+let cartLength = 0;
+let deleteBtns = [];
+let index = 0;
+let quant = +quantity.innerText;
+
+
+const openMenu = () => {
     let open = navMenu.classList.contains('open');
     navMenu.classList.toggle('open');
     if (!open) {
@@ -16,6 +37,92 @@ menuBtn.addEventListener('click', () => {
         menuBtn.setAttribute('aria-expanded', 'false');
         overlay.setAttribute('hidden', '')
     }
-    
-    
+}
+
+const showSlide = (n) => {
+    slides.forEach((slide, i) => {
+        slide.style.display = i === n ? 'block' : 'none'
+    })
+}
+showSlide(index)
+
+const changeSlides = (n) => {
+    index += n;
+    if (index >= slides.length) index = 0;
+    if (index < 0) index = slides.length - 1;
+    showSlide(index);
+}
+
+const disableMinusBtn = (n) => {
+    if (n === 0) {
+        minusQuantityBtn.setAttribute('disabled', '');
+        minusQuantityBtn.setAttribute('aria-disabled', 'true');
+        minusQuantityBtn.classList.add('disabled');
+    }else {
+        minusQuantityBtn.removeAttribute('disabled');
+        minusQuantityBtn.setAttribute('aria-disabled', 'false');
+        minusQuantityBtn.classList.remove('disabled');
+    }
+}
+
+const changeQuantity = (n) => {
+    quant += n;
+    quantity.innerText = quant;
+    disableMinusBtn(quant);
+}
+
+const updateCartUI = () => {
+    if (cartLength > 0) {
+        emptyMsg.setAttribute('hidden', '');
+        checkoutBtn.removeAttribute('hidden')
+        cartProducts.style.display = 'flex'
+    }else {
+        emptyMsg.removeAttribute('hidden');
+        checkoutBtn.setAttribute('hidden', '')
+        cartProducts.style.display = 'none'
+    }
+}
+
+const deletesProductFromCart = () => {
+    deleteBtns = document.querySelectorAll('#delete-button')
+    deleteBtns.forEach(btn => {
+        btn.addEventListener('click', () => {
+            btn.closest('div').remove()
+            cartLength--
+            updateCartUI()
+        })
+    })
+}
+
+const addsProductToCart = () => {
+    let cost = +prodCost.innerText
+    cartLength++
+    cartProducts.innerHTML += 
+    `<div class="entry">
+        <img src="./images/image-product-1-thumbnail.jpg" alt="" class="header__cart-img">
+        <div class="header__cart-labels">
+        <p>Fall Limited Edition Sneakers</p>
+        <p><span>$${cost}.00</span> x <span class="header__cart-quantity">${quant}</span><span class="header__cart-total tp3">$${(cost * quant)}.00</span></p>
+        </div>
+        <button class="header__cart-delete" id="delete-button"><img src="./images/icon-delete.svg" alt=""></button>
+    </div>`
+
+    deletesProductFromCart()
+    updateCartUI()
+}
+
+
+plusQuantityBtn.addEventListener('click', () => changeQuantity(1))
+minusQuantityBtn.addEventListener('click', () => changeQuantity(-1))
+
+menuBtn.addEventListener('click', () => {
+    openMenu()
+})
+
+addToCartBtn.addEventListener('click', () => {
+    addsProductToCart()
+})
+
+cartBtn.addEventListener('click', () => {
+    dialog.hasAttribute('open') ? dialog.removeAttribute('open') : dialog.setAttribute('open', '')
 })
