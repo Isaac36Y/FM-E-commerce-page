a1aa61501b7e90828204bc4ad3aa4b46e175b762
Isaac Young <isaac3.young6@gmail.com>
2026-02-02 18:46:14 -0800

    mobile finished with working functionality

--- script.js
+++ script.js
@@ -6,11 +6,8 @@ const dialog = document.querySelector('#shopping-cart')
 const cartBtn = document.querySelector('#cart-button');
 const cartProducts = document.querySelector('#cart-product-container');
 const checkoutBtn = document.querySelector('#checkout-button');
-<<<<<<< HEAD
 const prodCost = document.querySelector('#current-price');
 const emptyMsg = document.querySelector('.header__cart-empty');
-=======
->>>>>>> refs/remotes/origin/main
 
 const slides = document.querySelectorAll('.main__gallery-img');
 
@@ -19,25 +16,13 @@ const minusQuantityBtn = document.querySelector('.main__quantity-minus');
 const plusQuantityBtn = document.querySelector('.main__quantity-plus');
 const addToCartBtn = document.querySelector('#add-to-cart');
 
-
-<<<<<<< HEAD
-let cartLength = 0;
-let deleteBtns = [];
-let index = 0;
-let quant = +quantity.innerText;
-
-=======
-
-
-
-
+let cartLength = 0
 let deleteBtns;
 let index = 0;
 let quant = +quantity.innerText;
-let prodCost = document.querySelector('#current-price')
->>>>>>> refs/remotes/origin/main
 
-const openMenu = () => {
+
+const toggleMenu = () => {
     let open = navMenu.classList.contains('open');
     navMenu.classList.toggle('open');
     if (!open) {
@@ -85,7 +70,6 @@ const changeQuantity = (n) => {
     disableMinusBtn(quant);
 }
 
-<<<<<<< HEAD
 const updateCartUI = () => {
     if (cartLength > 0) {
         emptyMsg.setAttribute('hidden', '');
@@ -98,80 +82,34 @@ const updateCartUI = () => {
     }
 }
 
-const deletesProductFromCart = () => {
-    deleteBtns = document.querySelectorAll('#delete-button')
-    deleteBtns.forEach(btn => {
-        btn.addEventListener('click', () => {
-            btn.closest('div').remove()
-            cartLength--
-            updateCartUI()
-        })
-    })
+const deletesProductFromCart = (btn) => {
+    btn.closest('.entry').remove()
+    cartLength--
+    updateCartUI()
 }
 
 const addsProductToCart = () => {
     let cost = +prodCost.innerText
     cartLength++
-=======
-const addsProductToCart = () => {
-    let cost = +prodCost.innerText
-    const emptyMsg = document.querySelector('.header__cart-empty');
-    let empty = !emptyMsg.hasAttribute('hidden');
-    if (empty) {
-        emptyMsg.setAttribute('hidden', '');
-        checkoutBtn.removeAttribute('hidden')
-        cartProducts.style.display = 'flex'
-    };
 
->>>>>>> refs/remotes/origin/main
     cartProducts.innerHTML += 
     `<div class="entry">
         <img src="./images/image-product-1-thumbnail.jpg" alt="" class="header__cart-img">
         <div class="header__cart-labels">
         <p>Fall Limited Edition Sneakers</p>
-<<<<<<< HEAD
         <p><span>$${cost}.00</span> x <span class="header__cart-quantity">${quant}</span><span class="header__cart-total tp3">$${(cost * quant)}.00</span></p>
         </div>
-        <button class="header__cart-delete" id="delete-button"><img src="./images/icon-delete.svg" alt=""></button>
+        <button class="header__cart-delete" onclick="deletesProductFromCart(this)"><img src="./images/icon-delete.svg" alt=""></button>
     </div>`
 
-    deletesProductFromCart()
+
     updateCartUI()
 }
 
-
-=======
-        <p><span>${cost}</span> x <span class="header__cart-quantity">${quant}</span><span class="header__cart-total tp3">$${(cost * quant)}</span></p>
-        </div>
-        <button class="header__cart-delete" id="delete-button"><img src="./images/icon-delete.svg" alt=""></button>
-    </div>`
-}
-
-
-
-
-/* const deletesProductFromCart = (btn) => {
-    btn.closest(document.querySelector('.entry')).innerHTML = ``;
-} */
-
-
-
->>>>>>> refs/remotes/origin/main
-plusQuantityBtn.addEventListener('click', () => changeQuantity(1))
-minusQuantityBtn.addEventListener('click', () => changeQuantity(-1))
-
 menuBtn.addEventListener('click', () => {
-    openMenu()
+    toggleMenu()
 })
 
 addToCartBtn.addEventListener('click', () => {
     addsProductToCart()
-})
-
-cartBtn.addEventListener('click', () => {
-<<<<<<< HEAD
-    dialog.hasAttribute('open') ? dialog.removeAttribute('open') : dialog.setAttribute('open', '')
-=======
-    dialog.style.display = dialog.style.display === 'none' ? 'flex' : 'none'
->>>>>>> refs/remotes/origin/main
 })
